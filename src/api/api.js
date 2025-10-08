bfef3ac3a073312b588943ccc5bf9fec9686d067
KingSeraph07 <runsewemichael93@gmail.com>
2025-10-08 20:55:05 +0100

    uploaded backend

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 // src/api.js
 import axios from 'axios';
-// 'https://reports.whalesharkengineering.com.ng/api/'
+
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',  
-  //  baseURL:'https://reports.whalesharkengineering.com.ng/api/' ,
+  // baseURL: 'http://127.0.0.1:8000/api/',  
+   baseURL:'https://reports.whalesharkengineering.com.ng/api/' ,
 });
 
 export default API;
